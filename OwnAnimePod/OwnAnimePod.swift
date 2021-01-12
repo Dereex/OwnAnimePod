3f988c0d67965a6997e55ed32264145a3ca30408
Yaroslav Sikorskyi <yaroslav.sikorskyi@globallogic.com>
2021-01-12 17:51:23 +0200

    Add feature

---
+++ OwnAnimePod/OwnAnimePod.swift
@@ -0,0 +1,22 @@
+//
+//  OwnAnimePod.swift
+//  OwnAnimePod
+//
+//  Created by Yaroslav Sikorskyi on 12.01.2021.
+//
+
+import Foundation
+
+public final class OwnAnimePod {
+
+    let name = "OwnAnimePod"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
