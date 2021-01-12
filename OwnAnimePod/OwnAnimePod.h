a0775367c340d44adbf4929c736068e90ee0c67e
Yaroslav Sikorskyi <yaroslav.sikorskyi@globallogic.com>
2021-01-12 17:41:02 +0200

    Initial project setup

---
+++ OwnAnimePod/OwnAnimePod.h
@@ -0,0 +1,18 @@
+//
+//  OwnAnimePod.h
+//  OwnAnimePod
+//
+//  Created by Yaroslav Sikorskyi on 12.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OwnAnimePod.
+FOUNDATION_EXPORT double OwnAnimePodVersionNumber;
+
+//! Project version string for OwnAnimePod.
+FOUNDATION_EXPORT const unsigned char OwnAnimePodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OwnAnimePod/PublicHeader.h>
+
+
