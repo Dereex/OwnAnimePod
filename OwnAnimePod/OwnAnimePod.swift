5e91769770cb492b5ab083c670376f5f3448cc8b
Yaroslav Sikorskyi <yaroslav.sikorskyi@globallogic.com>
2021-01-13 10:31:21 +0200

    add kingfisher package

--- OwnAnimePod/OwnAnimePod.swift
+++ OwnAnimePod/OwnAnimePod.swift
@@ -6,17 +6,27 @@
 //
 
 import Foundation
+import Kingfisher
+import UIKit
 
 public final class OwnAnimePod {
 
-    let name = "OwnAnimePod"
+    static let name = "OwnAnimePod"
     
-    public func add(a: Int, b: Int) -> Int {
+    public static func add(a: Int, b: Int) -> Int {
         return a + b
     }
     
-    public func sub(a: Int, b: Int) -> Int {
+    public static func sub(a: Int, b: Int) -> Int {
         return a - b
+    }
+    
+    public static func kingfisher(completion: @escaping ((UIImageView) -> ())) {
+        let imageView = UIImageView()
+        imageView.kf.setImage(with: URL(string: "https://img.yakaboo.ua/media/catalog/product/cache/1/image/398x565/234c7c011ba026e66d29567e1be1d1f7/1/8/18_3_375.jpg")!) { _, _ in
+            
+            completion(imageView)
+        }
     }
     
 }
